5b68dec799c3f824b7d381e52e9acaf18ad7e0f5
leptone <leptone55@gmail.com>
2016-08-20 16:25:34 -0700

    stripped down to just server

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ var app = express()
 var bodyParser = require('body-parser')
 var request = require('request')
 
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
 app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
@@ -13,7 +10,7 @@ app.get('/', function(req, res) {
 })
 
 app.post('/', function(req, res) {
-    conscle.log('req.body', req.body)
+    console.log('req.body', req.body)
     var options = {
         uri: 'https://library.copperstud.io/ingest',
         method: 'POST',
@@ -33,7 +30,5 @@ app.post('/', function(req, res) {
         }
     })
 })
-
-app.use(express.static(__dirname + '/public'))
 
 app.listen(3000, function() { console.log('server running on port 3000') })
